c77606bf6b96ba168d0b0249c3229f0434e5f225
rancho <happyhaha628@qq.com>
2019-07-29 10:13:38 +0800

    ddd

--- filesprocess/settings.py
+++ filesprocess/settings.py
@@ -76,11 +76,12 @@ WSGI_APPLICATION = 'filesprocess.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-        'OPTIONS': {
-            'timeout': 20,
-        }
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'djangodatabase',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
     }
 }
 
