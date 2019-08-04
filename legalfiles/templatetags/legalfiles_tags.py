4db37e953181fa478dd4081dd7cd70c50339631e
rancho <happyhaha628@qq.com>
2019-08-04 18:13:19 +0800

    555

--- legalfiles/templatetags/legalfiles_tags.py
+++ legalfiles/templatetags/legalfiles_tags.py
@@ -33,3 +33,5 @@ def get_tags():
 
     return ret
 
+
+
--- legalfiles/urls.py
+++ legalfiles/urls.py
@@ -1,14 +1,14 @@
-from django.urls import path,include
+from django.urls import path, include
 
 import legalfiles.views
 
 app_name = 'legalfiles'
-urlpatterns=[
+urlpatterns = [
     path(r'to_upload/', legalfiles.views.to_upload, name='to_upload'),
     path(r'process_file/', legalfiles.views.process_file, name='process_file'),
     path(r'to_process/', legalfiles.views.to_process, name='to_process'),
     path(r'upload_file/', legalfiles.views.upload_file, name='upload_file'),
-    path(r'detail/<int:txt_id>',  legalfiles.views.get_detail_page,name='get_detail_page'),
-
-    path(r'index/',  legalfiles.views.IndexView.as_view(),name='index')
+    path(r'detail/<int:txt_id>', legalfiles.views.get_detail_page, name='get_detail_page'),
+    path(r'index/', legalfiles.views.IndexView.as_view(), name='index'),
+    path(r'^tag/(?P<pk>[0-9]+)/$', legalfiles.views.TagView.as_view(), name='tag'),
 ]
